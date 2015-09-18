2b46cdbf3de4ed99900795fdc2b3b085b799a197
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-17 20:22:19 -0400

    Add has_secure_password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :alerts, class_name: 'Alert', foreign_key: :mechanic_id
   has_many :created_alerts, class_name: 'Alert', foreign_key: :creator_id
   has_many :alert_issues, through: :alerts
