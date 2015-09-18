a7cdd248ebf6ea3ae3c25cf5ab847fb7c441b6a9
Alexander <ahuang62wp7c@gmail.com>
2015-09-18 14:33:50 -0400

    add edit test for user controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 describe UsersController do
-  describe 'user access' do
-    before :each do
-      @user = create(:user)
-      session[:user_id] = @user.id
-    end
+  before :each do
+    @user = create(:user)
+    session[:user_id] = @user.id
 
-    describe '#new' do
+    describe 'GET #new' do
       it "assigns a new user" do
         get :new
         expect(assigns(:user)).to be_a_new(User)
@@ -16,22 +14,37 @@ describe UsersController do
       it "renders signup form" do
         get :new
         expect(response).to render_template :new
-        end
+      end
     end
 
-    # describe '#create' do
-    #   it "creates a new user with valid attributes" do
-    #     get :new
-    #     binding.pry
-    #     post :create, user: @user_attributes
-    #     expect(User.last.username).to eq(@user.username)
-    #     expect(User.last.email).to eq(@user.email)
-    #   end
-    # end
+    describe 'POST #create' do
+      it "creates a new user with valid attributes" do
+        expect{
+            get :new
+            post :create, user: attributes_for(@user)
+        }.to change(User, :count).by(1)
+      end
 
-    # it "POST#create denies access" do
-    #   post :create, user: attributes_for(:user)
-    #   expect(response).to redirect_to root_url
-    # end
+      # it "redirects to alerts index page" do
+      #   post :create, user: attributes_for(:user)
+      #   expect(response).to redirect_to alerts_path
+      # end
+    end
+
+    describe 'GET #edit' do
+      it 'assigns the requested user to @user' do
+        get :edit, id: @user.id
+        expect(assigns(:user)).to eq @user
+      end
+
+      it 'renders the edit template' do
+        expect(response).to render_template :edit
+      end
+    end
   end
+
+  # describe 'PATCH #update' do
+  #   it
+  # end
+
 end
