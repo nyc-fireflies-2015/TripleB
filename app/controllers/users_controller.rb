aa9e94677e80ff1eda92d194c55c6902bbf60c85
GLNRO <lg@glnro.com>
2015-09-18 10:32:06 -0400

    Create users controller skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,11 @@
-users_controller.rb
+class UsersController < ApplicationController
+
+  def create
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username,:password,:first_name,:last_name,:email,:phone)
+  end
+
+end
