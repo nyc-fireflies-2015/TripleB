ecf47888b482e2e00a1ab2efbc1810a8c7a9070d
Alexander <ahuang62wp7c@gmail.com>
2015-09-18 11:12:44 -0400

    reformat new show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
   get 'login' => 'sessions#new'
-  get 'signup' => 'user#new'
+  get 'signup' => 'users#new'
 
   resources :users
   resources :alerts
