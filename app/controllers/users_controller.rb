c684415988906673d34562014f6f2b3fc11d87ce
Alexander <ahuang62wp7c@gmail.com>
2015-09-18 11:32:40 -0400

    create user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,10 @@ class UsersController < ApplicationController
   def create
   	user = User.new(user_params)
   	if user.save
-  		session[:user_id] = @user.id
+  		session[:user_id] = user.id
   		redirect_to alerts_path
   	else
-  		redirect_to new_user_path
+  		redirect_to '/signup'
   	end
   end
 
