a2c73b1aaa38f11c4217fb7f6c46174492ce07ee
emjr <emjrdev@gmail.com>
2015-09-18 15:21:42 -0400

    add rendering tests

--- spec/controllers/alerts_controller_spec.rb
+++ spec/controllers/alerts_controller_spec.rb
@@ -3,43 +3,22 @@ require 'rails_helper'
 RSpec.describe AlertsController, type: :controller do
 
   describe "GET #show" do
-    let!(:alert) { FactoryGirl.create(:alert) }
-
-    before :each do
-      get :show, id: alert.id
-    end
-
-    context 'user is not logged in' do
-    end
-
-    context 'user is logged in' do
-      it "assigns the requested alert to @alert" do
-        expect(assigns(:alert)).to eq alert
-      end
-      it "renders the :show template" do
-        expect(response).to render_template :show
-      end
+    it "renders the :show template" do
+      expect(response).to render_template :show
     end
   end
 
   describe "GET #new" do
-    let!(:user) { FactoryGirl.create(:user) }
-    let!(:alert) { FactoryGirl.create(:alert)}
-
-    context 'user is not logged in' do
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
     end
+  end
 
-    context 'user is logged in' do
-      it "assigns an empty alert to @alert" do
-        login(user)
-        get :new
-        expect(assigns(user.alert)).to be_a_new Alert
-      end
-      it "renders the :new template" do
-        login(user)
-        get :new
-        expect(response).to render_template :new
-      end
+  describe "GET #index" do
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end
   end
 
