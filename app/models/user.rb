0ff0d08758bfed49d27d0c1b88ed102fb1a21a6d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-17 23:43:29 -0400

    Add username uniqueness validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ActiveRecord::Base
   has_many :issues, through: :alert_issues
 
   validates :first_name, :last_name, :username, :email, :phone,  presence: true
-  validates :email, uniqueness: true
+  validates :email, :username, uniqueness: true
 end
