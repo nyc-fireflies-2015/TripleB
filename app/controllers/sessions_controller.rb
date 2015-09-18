e0571ac18f2dd517aee97b0d0af784a20393bd66
GLNRO <lg@glnro.com>
2015-09-18 10:48:33 -0400

    Add new route to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def create
     user = User.find_by(username: session_params[:username])
     if user && user.authenticate(session_params[:username])
