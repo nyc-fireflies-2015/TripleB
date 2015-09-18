80159508ec06786da2470629abf0d8f683d7ebb0
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-17 23:38:25 -0400

    Add duplicate email spec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,4 +47,11 @@ describe User do
     expect(user.errors[:password]).to include("can't be blank")
   end
 
+  it 'is invalid with a duplicate email address' do
+    create(:user, email: "test@test.com")
+    user = build(:user, email: "test@test.com")
+    user.valid?
+    expect(user.errors[:email]).to include("has already been taken")
+  end
+
 end
