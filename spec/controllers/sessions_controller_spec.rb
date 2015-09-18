588a64be87724dc7bcea652560c6569842a12550
GLNRO <lg@glnro.com>
2015-09-18 10:48:52 -0400

    Add login spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,6 +3,10 @@ require 'rails_helper'
 describe SessionsController  do
 
   describe "GET #new" do
+    it "redirects to login form" do
+      get :new
+      expect(response).to redirect_to login_path
+    end
   end
 
   describe "POST #create" do
