14485d5896f262b411ce42c51ea4830f3a84ce6d
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-17 23:31:28 -0400

    Add username validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,4 +29,10 @@ describe User do
     expect(user.errors[:username]).to include("can't be blank")
   end
 
+  it 'is invalid without a email' do
+    user = build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+
 end
