3f490e42563a8da9d941c7e1ae8c2058a8fc77cf
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-18 14:35:34 -0400

    Change login_helper spec targets to nav instead of div id

--- spec/support/login_helpers.rb
+++ spec/support/login_helpers.rb
@@ -2,7 +2,7 @@ module LoginHelpers
 
   def log_in(user)
     visit root_path
-    first('#login_header').click_link("Login")
+    first('nav').click_link("Login")
     fill_in "Username", with: user.username
     fill_in "Password", with: user.password
     click_button "Login"
