e8eb7cd60d93b9994ef704868914dc678e7191ec
GLNRO <lg@glnro.com>
2015-09-18 11:27:56 -0400

    Fix bug in authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    binding.pry
-    if user && user.authenticate(params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to alerts_path
     else
