c97679713c5c2bc89ba1925027fdb6fce3289241
GLNRO <lg@glnro.com>
2015-09-18 08:02:37 -0400

    Write sessions controller

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,1 +1,23 @@
-sessions.rb
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(username: session_params[:username])
+    if user && user.authenticate(session_params[:username])
+      session[:user_id] = user.id
+      redirect_to
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
+  private
+  def session_params
+    params.require(:session).permit(:username,:password)
+  end
+
+end
