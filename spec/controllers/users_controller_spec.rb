584d5f670f9a5f3ad3d73da0d4abcd2dd98de04e
Alexander <ahuang62wp7c@gmail.com>
2015-09-18 13:19:51 -0400

    create user valid attributes test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,32 @@
-users_controller_spec.rb
+require 'rails_helper'
+
+describe UsersController do
+  describe 'user access' do
+    before :each do
+      @user = create(:user)
+      session[:user_id] = @user.id
+    end
+
+    describe '#new' do
+      it "assigns a new user" do
+        get :new
+        expect(assigns(:user)).to be_a_new(User)
+      end
+    end
+
+    describe '#create' do
+      it "creates a new user with valid attributes" do
+        get :new
+        binding.pry
+        post :create, user: @user_attributes
+        expect(User.last.username).to eq(@user.username)
+        expect(User.last.email).to eq(@user.email)
+      end
+    end
+
+    # it "POST#create denies access" do
+    #   post :create, user: attributes_for(:user)
+    #   expect(response).to redirect_to root_url
+    # end
+  end
+end
