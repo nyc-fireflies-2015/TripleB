bf22d8bf4a1732c06d7f95f3e5a5b5863f34728a
GLNRO <lg@glnro.com>
2015-09-18 10:57:54 -0400

    Add redirect for sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: session_params[:username])
     if user && user.authenticate(session_params[:username])
       session[:user_id] = user.id
-      redirect_to
+      redirect_to alerts_path
     else
       redirect_to login_path
     end
