80c1c92be5b081f07c8b5bde2f06dc8806c69130
Alexander <ahuang62wp7c@gmail.com>
2015-09-18 16:43:54 -0400

    finish user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 describe UsersController do
 
   describe 'GET #new' do
-
-    it "assigns a new user" do
+    before :each do
       @user = create(:user)
       get :new
+    end
+
+    it "assigns a new user" do
       expect(assigns(:user)).to be_a_new(User)
     end
 
     it "renders signup form" do
-      @user = create(:user)
-      get :new
       expect(response).to render_template :new
     end
   end
@@ -20,8 +20,8 @@ describe UsersController do
   describe 'POST #create' do
     it "creates a new user with valid attributes" do
       expect{
-          get :new
-          post :create, user: attributes_for(:user)
+        get :new
+        post :create, user: attributes_for(:user)
       }.to change(User, :count).by(1)
     end
 
@@ -40,11 +40,38 @@ describe UsersController do
   end
 
   describe 'GET #edit' do
-    it 'renders the edit template' do
+    before :each do
       @user = create(:user)
-      get :edit, id: @user
+      session[:user_id] = @user.id
+      get :edit, id: @user.id
+    end
+
+    it 'assigns the requested user to @user' do
+      expect(assigns(:user)).to eq @user 
+    end
+
+    it 'renders the edit template' do
       expect(response).to render_template :edit
     end
   end
 
+  describe 'PATCH #update' do
+    before :each do
+      @user = create(:user, first_name: 'John')
+      session[:user_id] = @user.id
+      get :edit, id: @user
+    end
+
+    it 'updates a user\'s attributes' do
+      patch :update, id: @user.id, user: attributes_for(:user, first_name: 'Billy')
+      @user.reload
+      expect(@user.first_name).to eq 'Billy'
+    end
+
+    it 'redirects to the user\'s profile' do
+      patch :update, id: @user.id, user: attributes_for(:user)
+      @user.reload
+      expect(response).to redirect_to @user
+    end
+  end
 end
