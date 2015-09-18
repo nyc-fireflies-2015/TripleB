0957702a63b85c31117fbf0a26d41fffbb4390c6
GLNRO <lg@glnro.com>
2015-09-18 12:25:44 -0400

    Add branch changeS

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < ApplicationController
 
-<<<<<<< HEAD
   def new
   end
 
@@ -9,13 +8,6 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to alerts_path
-=======
-  def create
-    user = User.find_by(username: session_params[:username])
-    if user && user.authenticate(session_params[:username])
-      session[:user_id] = user.id
-      redirect_to
->>>>>>> 767ec1b0d029ec3e2cd00909f2c8968d237839d1
     else
       redirect_to login_path
     end
@@ -25,13 +17,5 @@ class SessionsController < ApplicationController
     session.clear
     redirect_to root_path
   end
-<<<<<<< HEAD
-=======
 
-  private
-  def session_params
-    params.require(:session).permit(:username,:password)
-  end
-
->>>>>>> 767ec1b0d029ec3e2cd00909f2c8968d237839d1
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,13 +1,9 @@
 require 'rails_helper'
-<<<<<<< HEAD
 include AuthenticationHelpers
-=======
->>>>>>> 767ec1b0d029ec3e2cd00909f2c8968d237839d1
 
 describe SessionsController  do
 
   describe "GET #new" do
-<<<<<<< HEAD
     it "renders login form" do
       get :new
       expect(response).to render_template :new
@@ -34,14 +30,6 @@ describe SessionsController  do
       expect(response).to redirect_to root_path
       expect(session[:user_id]).to eq(nil)
     end
-=======
-  end
-
-  describe "POST #create" do
-  end
-
-  describe "DELETE #destroy" do
->>>>>>> 767ec1b0d029ec3e2cd00909f2c8968d237839d1
   end
 
 end
