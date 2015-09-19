57174d046b057a0621f2db823530f9839318dd46
GLNRO <lg@glnro.com>
2015-09-19 15:24:47 -0400

    Add updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,42 @@
+user_1 = User.create(first_name: "Maggie", last_name: "Thatcher",username: "maggie_420", email: "maggiethatch@gmail.com", phone: "9999999999", password: "1234")
 5.times do
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,username: Faker::Internet.user_name, email: Faker::Internet.free_email, phone: Faker::PhoneNumber.phone_number, password: "1234")
 end
 
-issues = ["worn brake pad","slipped chain","detached brake cable","punctured tube", "popped tire", "loose crankset","split fork"]
+tags = ["brake pad","chain","brake cable","tube", "tire", "crankset","fork"]
 
-issue_obj = issues.each {|issue| Issue.create(part: issue)}
+tag_obj = tags.each {|tag| Tag.create(name: tag)}
 
 status = ["incomplete","in progress","complete"]
 
 users = User.all
 
 2.times do
-  alert = users[0].created_alerts.create(latitude: 40.672203,longitude: -73.989809,status: status[0])
-  alert.alert_issues.create(issue_id: 1, description: "brake pads are balding")
+  alert = users[0].created_alerts.create(latitude: 40.672203,longitude: -73.989809,status: status[0], description: "brake pads are balding")
+  alert.alert_tags.create(tag_id: 1)
 end
 2.times do
-  alert = users[1].created_alerts.create(latitude: 40.681031,longitude: -73.974717,status: status[1])
+  alert = users[1].created_alerts.create(latitude: 40.681031,longitude: -73.974717,status: status[1], description: "my chain slipped off idk what to do")
   alert.mechanic = users[2]
-  alert.alert_issues.create(issue_id: 2, description: "my chain slipped off idk what to do")
+  alert.alert_tags.create(tag_id: 2)
 end
 2.times do
-  alert = users[0].created_alerts.create(latitude: 40.689300,longitude: -73.957153,status: status[2])
+  alert = users[0].created_alerts.create(latitude: 40.689300,longitude: -73.957153,status: status[2],description: "busted brake cable, can't stop!")
   alert.mechanic = users[2]
-  alert.alert_issues.create(issue_id: 3, description: "busted brake cable, can't stop!")
+  alert.alert_tags.create(tag_id: 3)
 end
 2.times do
-  alert = users[3].created_alerts.create(latitude: 40.721215,longitude: -73.962044,status: status[0])
-  alert.alert_issues.create(issue_id: 4, description: "rode over a rock, need an extra tube")
+  alert = users[3].created_alerts.create(latitude: 40.721215,longitude: -73.962044,status: status[0],description: "rode over a rock, need an extra tube")
+  alert.alert_tags.create(tag_id: 4)
 end
 2.times do
-  alert = users[4].created_alerts.create(latitude: 40.711171,longitude: -73.960916,status: status[1])
-  alert.mechanic = users[1]
-  alert.alert_issues.create(issue_id: 5, description: "rode through a tack convention and now my tire is flat :(")
+  alert = users[4].created_alerts.create(latitude: 40.711171,longitude: -73.960916,status: status[1], description: "rode through a tack convention and now my tire is flat :(")
+  alert.mechanic = users[5]
+  alert.alert_tags.create(tag_id: 5)
 end
 2.times do
-  alert = users[4].created_alerts.create(latitude: 40.752761,longitude: -73.975263,status: status[2])
-  alert.mechanic = users[0]
-  alert.alert_issues.create(issue_id: 7, description: "my bike split in half where is the nearest repair shop!??")
+  alert = users[4].created_alerts.create(latitude: 40.752761,longitude: -73.975263,status: status[2], description: "my bike split in half where is the nearest repair shop!??")
+  alert.mechanic = users[6]
+  alert.alert_tags.create(tag_id: 7)
 
 end
