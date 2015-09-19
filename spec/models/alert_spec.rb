a03c2864f8cc0330c03486d1cd60f7b13b2eb7dd
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-19 13:22:35 -0400

    Remove association specs

--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -1,21 +1,6 @@
 require 'rails_helper'
 
 describe Alert do
-  context 'associations' do
-
-    it 'should belong to creator (class name User)' do
-      should belong_to(:creator).class_name("User")
-    end
-
-    it 'should belong to mechanic (class name User)' do
-      should belong_to(:mechanic).class_name("User")
-    end
-
-    it 'should have many tags' do
-      should have_many :tags
-    end
-  end
-
   context 'validations' do
 
     it 'has a valid factory' do
