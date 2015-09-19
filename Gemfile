31fbb610af5eb13cb80b78540ebf8f924346ad6c
emjr <emjrdev@gmail.com>
2015-09-19 13:49:38 -0400

    implement time difference

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
 gem 'pry-rails', :group => :development
+gem 'time_difference'
 
 group :development, :test do
 
--- app/models/alert.rb
+++ app/models/alert.rb
@@ -3,4 +3,16 @@ class Alert < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :alert_issues
   has_many :issues, through: :alert_issues
+
+  def time_diff
+    start_time = self.created_at
+    end_time = DateTime.now
+    full_diff = TimeDifference.between(start_time, end_time).in_general
+    if full_diff[:hours]
+      "#{full_diff[:hours]} hours and #{full_diff[:minutes]} minutes ago"
+    else
+      "#{full_diff[:minutes]} minutes ago"
+    end
+  end
+
 end
