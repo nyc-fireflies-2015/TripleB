7e3bd625a778f012a8119d7975492451cc2fe49c
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-19 12:59:54 -0400

    Remove obsolete join table spec, test factory validation

--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Alert do
   context 'associations' do
-    
+
     it 'should belong to creator (class name User)' do
       should belong_to(:creator).class_name("User")
     end
@@ -11,13 +11,17 @@ describe Alert do
       should belong_to(:mechanic).class_name("User")
     end
 
-    it 'should have many alert_issues' do
-      should have_many :alert_issues
-    end
 
     it 'should have many issues' do
       should have_many :issues
     end
-
   end
+
+  context 'validations' do
+
+    it 'has a valid factory' do
+      expect(build(:alert)).to be_valid
+    end
+  end
+
 end
