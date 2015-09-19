530aa7a9195f08aa6a927709296734394c8537c2
GLNRO <lg@glnro.com>
2015-09-19 17:05:01 -0400

    Add event listener for alert form submit

--- app/assets/javascripts/alert_geolocation.js
+++ app/assets/javascripts/alert_geolocation.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
+  <p id="lat_long"></p>
+
+  <button onclick= "getLocation()">get location</button>
+  var x = document.getElementById("lat_long");
+
+  $('#alert_form').on("submit",function(){
+    function getLocation(){
+      if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+      }
+      else {
+        x.innerHTML = "geolocation not supported"
+      }
+    }
+
+    function showPosition(position){
+      x.innerHTML = "Latitude:" + position.coords.latitude +"<br>Longitude:" + position.coords.longitude;
+    }
+
+    getLocation();
+
+  })
 
 });
