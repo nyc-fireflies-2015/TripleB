4513b3cf5941f99ef2541ba0d258f4800e81c615
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-19 13:21:25 -0400

    Removed obsolete models

--- app/models/alert_issue.rb
+++
@@ -1,4 +0,0 @@
-class AlertIssue < ActiveRecord::Base
-  belongs_to :alert
-  belongs_to :issue
-end
--- app/models/issue.rb
+++
@@ -1,4 +0,0 @@
-class Issue < ActiveRecord::Base
-  has_many :alert_issues
-  has_many :alerts, through: :alert_issues
-end
