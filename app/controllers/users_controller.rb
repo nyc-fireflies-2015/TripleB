ae3819cc42eac975e5f72c9103da622bdea16994
Alexander <ahuang62wp7c@gmail.com>
2015-09-19 16:16:20 -0400

    add action mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,27 @@ class UsersController < ApplicationController
 	end
 
   def create
-  	user = User.new(user_params)
-  	if user.save
-  		session[:user_id] = user.id
-  		redirect_to alerts_path
-  	else
-  		redirect_to '/signup'
-  	end
+  	@user = User.new(user_params)
+  	# if user.save
+  	# 	session[:user_id] = user.id
+  	# 	redirect_to alerts_path
+  	# else
+  	# 	redirect_to '/signup'
+  	# end
+
+    respond_to do |format|
+      if @user.save
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.welcome_email(@user).deliver
+        session[:user_id] = @user.id
+ 
+        format.html { redirect_to alerts_path }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
