b26eeaf086e561bca4f5c4ba6f92103e837b900f
GLNRO <lg@glnro.com>
2015-09-19 19:59:21 -0400

    get location function working

--- app/assets/javascripts/alert_geolocation.js
+++ app/assets/javascripts/alert_geolocation.js
@@ -1,24 +1,20 @@
 $(document).ready(function(){
-  <p id="lat_long"></p>
 
-  <button onclick= "getLocation()">get location</button>
-  var x = document.getElementById("lat_long");
-  var latitutde;
-  var longitude;
 
-  $('#alert_form').load(function(){
+  $(window).load(function(){
     function getLocation(){
       if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition)
       }
       else {
-        x.innerHTML = "geolocation not supported"
+
       }
     }
 
     function showPosition(position){
-      latitude = position.coords.latitude;
+      latitude = position.coords.latitude);
       longitude = position.coords.longitude;
+      $('#location').text(latitude);
     }
     getLocation();
 
