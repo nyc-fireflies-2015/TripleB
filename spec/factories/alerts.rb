94b333a21f8e3b884ea33bcdaed8d027c518e263
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-19 12:59:11 -0400

    Add correct association to alert factory with creator

--- spec/factories/alerts.rb
+++ spec/factories/alerts.rb
@@ -2,8 +2,9 @@ require 'faker'
 
 FactoryGirl.define do
   factory :alert do
-    creator_id { 1 }
+    association :creator, factory: :user
     latitude { Faker::Address.latitude }
     longitude { Faker::Address.longitude }
+    description { Faker::Lorem.paragraph }
   end
 end
