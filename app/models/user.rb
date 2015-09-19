fb08572c94c40477c6dd27ad3c414469cbda5f06
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-19 13:21:50 -0400

    Fixed has many association in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :alerts, class_name: 'Alert', foreign_key: :mechanic_id
   has_many :created_alerts, class_name: 'Alert', foreign_key: :creator_id
+  has_many :alert_tags
   has_many :tags, through: :alert_tags
 
   validates :first_name, :last_name, :username, :email, :phone,  presence: true
