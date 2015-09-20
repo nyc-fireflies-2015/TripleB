b6e62140851fe798140c593fadc6561f542452c7
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-20 19:04:15 -0400

    Add bike path route on in progress map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,3 +1,4 @@
+
 function initMap() {
   var lat = $('#lat').text();
   var lng = $('#lng').text();
@@ -6,11 +7,18 @@ function initMap() {
   var myLatLng = {lat: Number(lat), lng: Number(lng)};
   var mechLatLng = {lat: Number(latMech), lng: Number(lngMech)};
   // Create a map object and specify the DOM element for display.
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
   var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
     center: myLatLng,
     scrollwheel: false,
     zoom: 14
   });
+
+  directionsDisplay.setMap(map);
+
+  calculateAndDisplayRoute(directionsService, directionsDisplay, mechLatLng, myLatLng);
+
 
   var marker = new google.maps.Marker({
     position: myLatLng,
@@ -27,6 +35,20 @@ function initMap() {
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 }
+
+function calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination) {
+  directionsService.route({
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.BICYCLING
+  }, function(response, status) {
+    if (status === google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions request failed due to ' + status);
+    }
+  });
+};
 
 document.addEventListener('DOMContentLoaded', function(){
   initMap();
