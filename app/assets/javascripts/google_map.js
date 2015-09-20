a276ccd447f735d2304a7ddc784aa5f943811046
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-20 17:12:08 -0400

    Add mechanic location marker to google map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,18 +1,27 @@
 function initMap() {
   var lat = $('#lat').text();
   var lng = $('#lng').text();
+  var latMech = $('#lat_mech').text();
+  var lngMech = $('#lng_mech').text();
   var myLatLng = {lat: Number(lat), lng: Number(lng)};
+  var mechLatLng = {lat: Number(latMech), lng: Number(lngMech)};
   // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
     center: myLatLng,
     scrollwheel: false,
-    zoom: 15
+    zoom: 14
   });
 
   var marker = new google.maps.Marker({
     position: myLatLng,
     map: map,
     title: 'Hello World!'
+  });
+
+  var mechMarker = new google.maps.Marker({
+    position: mechLatLng,
+    map: map,
+    title: "sup"
   });
 
   var bikeLayer = new google.maps.BicyclingLayer();
