7c38fe350a2adb68bff8c6a4f68f3a5898e42b3a
Alexander <ahuang62wp7c@gmail.com>
2015-09-20 15:11:53 -0400

    WIP actionmailer setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
  
   def welcome_email(user)
     @user = user
-    @url  = 'http://example.com/login'
+    @url  = 'http://localhost:3000/login'
     mail(to: @user.email, subject: 'Welcome to My Awesome Site')
   end
 end
