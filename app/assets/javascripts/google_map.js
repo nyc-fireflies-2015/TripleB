33c2534a834aa56ba7cfaf94b58a48dd96bfb1ef
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-20 19:55:02 -0400

    Add location map js

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -2,22 +2,13 @@
 function initMap() {
   var lat = $('#lat').text();
   var lng = $('#lng').text();
-  var latMech = $('#lat_mech').text();
-  var lngMech = $('#lng_mech').text();
   var myLatLng = {lat: Number(lat), lng: Number(lng)};
-  var mechLatLng = {lat: Number(latMech), lng: Number(lngMech)};
   // Create a map object and specify the DOM element for display.
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-  var map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+  var map = new google.maps.Map(document.getElementsByClassName('location_map')[0], {
     center: myLatLng,
     scrollwheel: false,
     zoom: 14
   });
-
-  directionsDisplay.setMap(map);
-
-  calculateAndDisplayRoute(directionsService, directionsDisplay, mechLatLng, myLatLng);
 
 
   var marker = new google.maps.Marker({
@@ -26,29 +17,10 @@ function initMap() {
     title: 'Hello World!'
   });
 
-  var mechMarker = new google.maps.Marker({
-    position: mechLatLng,
-    map: map,
-    title: "sup"
-  });
 
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
 }
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination) {
-  directionsService.route({
-    origin: origin,
-    destination: destination,
-    travelMode: google.maps.TravelMode.BICYCLING
-  }, function(response, status) {
-    if (status === google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(response);
-    } else {
-      window.alert('Directions request failed due to ' + status);
-    }
-  });
-};
 
 document.addEventListener('DOMContentLoaded', function(){
   initMap();
