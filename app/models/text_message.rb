b3603cab66bc1a85c5f43d1117a85b76fa53e2f8
GLNRO <lg@glnro.com>
2015-09-20 11:32:38 -0400

    Change sid/auth token for real world test

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -1,16 +1,16 @@
 class TextMessage < ActiveRecord::Base
 
-  def self.send(mechanic_phone)
-    twilio_sid = 'ACd6d900e07f5493c3464583803fbbf7a7'
-    twilio_token = '86a4f4637d6d43209027ea628c885e49'
+  def self.send
+    twilio_sid = 'ACd5d81589e2835930ec8dd5d2099d4e93'
+    twilio_token = '5c05cc9d90d20d5a5968d6a848b5d9cb'
     twilio_phone_number = '+19292003142'
-    @client = Twilio::Rest::Client.new(twilio_sid,twilio_token)
+    @client = Twilio::REST::Client.new(twilio_sid,twilio_token)
 
-    @client.account.sms.messages.create(
-      from: twilio_phone_number,
-      to: mechanic_phone,
-      body: "This is a test from george"
-      )
+    @client.account.messages.create({
+      :from => '+19292003142',
+      :to => '+19542950857',
+      :body => 'hello this is another test',
+})
   end
 
 end
