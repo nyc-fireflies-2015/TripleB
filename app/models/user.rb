c9688dd3d5d6f3e6f8f80cef73672e06fc6be836
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-20 15:14:58 -0400

    Add full name method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
 
   validates :first_name, :last_name, :username, :email, :phone,  presence: true
   validates :email, :username, uniqueness: true
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
