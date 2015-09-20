92c6de0f98ca6e2ec0a4a86565b6ecde71243ab5
emjr <emjrdev@gmail.com>
2015-09-20 19:11:22 -0400

    add phone and email format validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,9 @@ class User < ActiveRecord::Base
   has_many :tags, through: :alert_tags
 
   validates :first_name, :last_name, :username, :email, :phone,  presence: true
-  validates :email, :username, :phone, : uniqueness: true
+  validates :email, :username, :phone, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "numbers should be entered with dashes" }
 
   def full_name
     [first_name, last_name].join(' ')
