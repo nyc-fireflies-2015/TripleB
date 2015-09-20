1e39c09d2d3dfe61f0e679c2455d0eadbb790271
Gp3gp3gp3 <gpaul.empyrean@gmail.com>
2015-09-20 13:55:23 -0400

    Remove unnessicary and redudant routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'login' => 'sessions#new'
   get 'signup' => 'users#new'
-  get 'profile' => 'users#show'
 
-  resources :users
+  resources :users, except: :new
   resources :alerts
-  resources :sessions
+  resources :sessions, except: :new
 
 end
