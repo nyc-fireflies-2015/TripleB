9b967795c57974cdb9e6cc18547270c42e017f06
Alexander <ahuang62wp7c@gmail.com>
2015-09-20 00:39:50 -0400

    add time difference test

--- spec/models/alert_spec.rb
+++ spec/models/alert_spec.rb
@@ -28,4 +28,15 @@ describe Alert do
 
   end
 
+  context 'model methods' do
+
+    it 'calculates the time difference' do
+      alert = build(:alert)
+      alert.update_attributes(created_at: Time.current - 3.5.hours)
+      # require 'pry'; binding.pry
+      expect(alert.time_diff).to eq '3 hours and 30 minutes ago'
+    end
+
+  end
+
 end
