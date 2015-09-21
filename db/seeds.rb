a64bcb756df1bbadaf74116c081d5687d1797697
GLNRO <lg@glnro.com>
2015-09-20 20:50:22 -0400

    Add valid phone number for twilio testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 5.times do
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,username: Faker::Internet.user_name, email: Faker::Internet.free_email, phone: Faker::PhoneNumber.phone_number, password: "1234")
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,username: Faker::Internet.user_name, email: Faker::Internet.free_email, phone: "9542950857", password: "1234")
 end
 
 tags = ["brake pad","chain","brake cable","tube", "tire", "crankset","fork"]
