f7468603757f41fb51422d94b302389f34391bd1
GLNRO <lg@glnro.com>
2015-09-20 21:31:57 -0400

    Remove phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :tags, through: :alert_tags
 
   validates :first_name, :last_name, :username, :email, :phone,  presence: true
-  validates :email, :username, :phone, uniqueness: true
+  validates :email, :username, uniqueness: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 
   def full_name
