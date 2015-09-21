0dc96dd7eb502e3f512c1b1428fa9524ec7befbc
GLNRO <lg@glnro.com>
2015-09-20 21:31:40 -0400

    Separate config method

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -1,26 +1,27 @@
 class TextMessage < ActiveRecord::Base
 
-  def initialize(distance)
+  def self.config
     twilio_sid = 'ACd5d81589e2835930ec8dd5d2099d4e93'
     twilio_token = '5c05cc9d90d20d5a5968d6a848b5d9cb'
     twilio_phone_number = '+19292003142'
     @client = Twilio::REST::Client.new(twilio_sid,twilio_token)
-    @distance = distance
   end
 
-  def send_to_mechanic(user_number,user)
+  def self.send_to_mechanic(user,user_number,distance)
+    self.config
     @client.account.messages.create({
       :from => '+19292003142',
       :to => '+1' + user_number,
-      :body => "Thanks for offering to help, you are {@distance} away from {user}. Get in touch with them by texting {user_number}",
+      :body => "Thanks for offering to help, you are #{distance} away from #{user}. Get in touch with them by texting #{user_number}",
     })
   end
 
-  def send_to_user(mech_number,mechanic)
+  def self.send_to_user(mechanic,mech_number,distance)
+    self.config
     @client.account.messages.create({
       :from => '+19292003142',
       :to => '+1' + mech_number,
-      :body => "Help is on the way, {mechanic} is {@distance} away. Get in touch with them by texting {mech_number}",
+      :body => "Help is on the way, #{mechanic} is #{distance} away. Get in touch with them by texting #{mech_number}",
     })
   end
 
