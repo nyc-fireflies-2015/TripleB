f3e57e2b72a354edd202648cc83c9d8d20c13bc1
GLNRO <lg@glnro.com>
2015-09-22 16:36:54 -0400

    Remove unecessary method

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,13 +7,4 @@ class Location < ActiveRecord::Base
                    :lat_column_name => :latitude,
                    :lng_column_name => :longitude
 
-  def self.get_alerts
-  	alerts = self.each_with_object([]) do |location, array|
-  		if location.locatable_type == "Alert"
-  			alerts << location.locatable
-  		end
-  	end
-  	return alerts
-  end
-
 end
