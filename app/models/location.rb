c8ea378f43968b2bb0278cbfb4cb3f275a3f14df
GLNRO <lg@glnro.com>
2015-09-22 17:38:26 -0400

    Add location_bearer association on create of location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,9 @@
 class Location < ActiveRecord::Base
 	belongs_to :locatable, polymorphic: true
-
+  after_create do
+    location_bearer = self.locatable_type.constantize.find(self.locatable_id)
+    location_bearer.update_attributes(location_id: self.id)
+  end
 	acts_as_mappable :default_units => :miles,
                    :default_formula => :sphere,
                    :distance_field_name => :distance,
