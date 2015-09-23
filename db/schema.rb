641190ccece3d4669664cc54f88b6b28cdfd9d80
Alexander <ahuang62wp7c@gmail.com>
2015-09-23 13:04:17 -0400

    pull from master

--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 20150921202142) do
   end
 
   create_table "locations", force: :cascade do |t|
-    t.float    "latitude"
-    t.float    "longitude"
+    t.float    "latitude",       null: false
+    t.float    "longitude",      null: false
     t.integer  "locatable_id"
     t.string   "locatable_type"
     t.datetime "created_at",     null: false
@@ -37,27 +37,27 @@ ActiveRecord::Schema.define(version: 20150921202142) do
   add_index "locations", ["locatable_type", "locatable_id"], name: "index_locations_on_locatable_type_and_locatable_id", using: :btree
 
   create_table "receipts", force: :cascade do |t|
-    t.integer  "alert_id"
-    t.integer  "mechanic_id"
+    t.integer  "alert_id",    null: false
+    t.integer  "mechanic_id", null: false
     t.integer  "location_id"
-    t.integer  "distance"
-    t.integer  "duration"
+    t.integer  "distance",    null: false
+    t.integer  "duration",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",                                                                                       null: false
-    t.string   "last_name",                                                                                        null: false
-    t.string   "username",                                                                                         null: false
-    t.string   "email",                                                                                            null: false
-    t.string   "phone",                                                                                            null: false
-    t.string   "password_digest",                                                                                  null: false
+    t.string   "first_name",                                                           null: false
+    t.string   "last_name",                                                            null: false
+    t.string   "username",                                                             null: false
+    t.string   "email",                                                                null: false
+    t.string   "phone",                                                                null: false
+    t.string   "password_digest",                                                      null: false
     t.string   "bio",             default: ""
-    t.string   "avatar_url",      default: "http://powerlearningny.com/wp-content/uploads/2014/04/blank_male.png"
+    t.string   "avatar_url",      default: "../../app/assets/images/blank_avatar.png"
     t.integer  "location_id"
-    t.datetime "created_at",                                                                                       null: false
-    t.datetime "updated_at",                                                                                       null: false
+    t.datetime "created_at",                                                           null: false
+    t.datetime "updated_at",                                                           null: false
   end
 
 end
