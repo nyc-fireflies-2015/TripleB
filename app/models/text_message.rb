f867789401d54e479f319e9b9aab279d9c24e3a6
emjr <emjrdev@gmail.com>
2015-09-23 10:37:45 -0400

    ENV fix

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -2,16 +2,16 @@
 class TextMessage < ActiveRecord::Base
 
   def self.config
-    twilio_sid = ENV[TWILIO_SID]
-    twilio_token = ENV[TWILIO_TOKEN]
-    twilio_phone_number = ENV[TWILIO_PHONE]
+    twilio_sid = ENV["TWILIO_SID"]
+    twilio_token = ENV["TWILIO_TOKEN"]
+    twilio_phone_number = ENV["TWILIO_PHONE"]
     @client = Twilio::REST::Client.new(twilio_sid,twilio_token)
   end
 
   def self.send_to_mechanic(user,mech_number,user_number,distance,duration)
     self.config
     @client.account.messages.create({
-      :from => ENV[TWILIO_PHONE],
+      :from => ENV["TWILIO_PHONE"],
       :to => '+1' + mech_number,
       :body => "Thanks for offering to help, you are #{distance} away from #{user}. Your trip duration is #{duration}. Get in touch with them by texting #{user_number}",
     })
@@ -20,7 +20,7 @@ class TextMessage < ActiveRecord::Base
   def self.send_to_user(mechanic,mech_number,user_number,distance,duration)
     self.config
     @client.account.messages.create({
-      :from => ENV[TWILIO_PHONE],
+      :from => ENV["TWILIO_PHONE"],
       :to => '+1' + user_number,
       :body => "Help is on the way, #{mechanic} is #{distance} away. Estimated trip time is #{duration} Get in touch with them by texting #{mech_number}.",
     })
