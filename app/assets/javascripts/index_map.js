0aec93a22cca1229838f5237d12e573c13e5eb8c
GLNRO <lg@glnro.com>
2015-09-23 11:19:07 -0400

    Refactor index maps

--- app/assets/javascripts/index_map.js
+++ app/assets/javascripts/index_map.js
@@ -1,41 +1,45 @@
-function newIndexMap(pos) {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementsByClassName('index_position_map')[0], {
-    center: pos,
-    scrollwheel: false,
-    zoom: 14
-  });
-
-
+function newIndexMap(pos, map) {
   var marker = new google.maps.Marker({
     position: pos,
     map: map,
     title: 'Hello World!'
   });
 
-  var indexLat = $('#alert_coord_lat').text();
-  var indexLng = $('#alert_coord_lng').text();
-
-  function convertCoords(coordArray){
-    var coords = coordArray;
-    split = coords.split(',');
-    arr = split.splice(1,split.length);
-    return arr.map(function(coord){ return Number(coord)});
-  }
-
-  function markers(lats,lngs){
-
-  }
-  markers()
-  var results = convertCoords(indexLat);
-  debugger
   var bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
+}
+
+var markers = function(lats,lngs,map){
+  var latitudes = lats
+  var longitudes = lngs
+  for (var i=0; i < lats.length; i++){
+    var coord = new google.maps.LatLng(lats[i],lngs[i]);
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map,
+      title: 'Hello World!'
+    })
+    marker.setMap(map);
+  }
 }
 
 document.addEventListener('DOMContentLoaded', function(){
   var lat = Number($('#current_user_lat').text());
   var lng = Number($('#current_user_lng').text());
-  var myLatLng = {lat: Number(lat), lng: Number(lng)};
-  newIndexMap(myLatLng);
+  var myPosition = {lat: Number(lat), lng: Number(lng)};
+
+
+  var map = new google.maps.Map(document.getElementsByClassName('index_position_map')[0], {
+    center: myPosition,
+    scrollwheel: false,
+    zoom: 14
+  });
+
+
+  newIndexMap(myPosition, map);
+
+  var latitudes = $('.latitude').map(function(index,item){ return Number($(item).text());})
+  var longitudes = $('.longitude').map(function(index,item){ return Number($(item).text());})
+
+  markers(latitudes,longitudes,map)
 })
