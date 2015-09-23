79702ab8c5ade90056feac05a7913ad9e8a159fe
Alexander <ahuang62wp7c@gmail.com>
2015-09-23 10:23:42 -0400

    add validations to migration tables

--- db/migrate/20150921202142_create_receipts.rb
+++ db/migrate/20150921202142_create_receipts.rb
@@ -1,11 +1,11 @@
 class CreateReceipts < ActiveRecord::Migration
   def change
     create_table :receipts do |t|
-    	t.references :alert
-    	t.references :mechanic
+    	t.references :alert, null: false
+    	t.references :mechanic, null: false
     	t.references :location
-    	t.integer :distance
-    	t.integer :duration
+    	t.integer :distance, null: false
+    	t.integer :duration, null: false
 
     	t.timestamps null: false
     end
